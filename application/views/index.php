eaf98fb4f95810092e588817d7f0bb8a9eb0c46d
amit.ja <amit.ja@directi.com>
2015-03-19 22:13:49 +0530

    1.included url library

--- application/views/index.php
+++ application/views/index.php
@@ -5,24 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Home | Corlate</title>
-	
+    <title>KRS Connect</title>
 	<!-- core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/animate.min.css" rel="stylesheet">
-    <link href="css/prettyPhoto.css" rel="stylesheet">
-    <link href="css/main.css" rel="stylesheet">
-    <link href="css/responsive.css" rel="stylesheet">
+    <link href="libs/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>application/views/libs/css/font-awesome.min.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>application/views/libs/css/animate.min.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>index.php/libs/css/prettyPhoto.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>index.php/libs/css/main.css" rel="stylesheet">
+    <link href="<?php echo base_url()?>index.php/libs/css/responsive.css" rel="stylesheet">
     <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-    <script src="js/respond.min.js"></script>
+    <script src="<?php echo base_url()?>libs/js/html5shiv.js"></script>
+    <script src="<?php echo base_url()?>libs/js/respond.min.js"></script>
     <![endif]-->       
-    <link rel="shortcut icon" href="images/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="<?php echo base_url()?>libs/images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="libs/images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="libs/images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="libs/images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="libs/images/ico/apple-touch-icon-57-precomposed.png">
 </head><!--/head-->
 
 <body class="homepage">
@@ -773,11 +772,11 @@
         </div>
     </footer><!--/#footer-->
 
-    <script src="js/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.prettyPhoto.js"></script>
-    <script src="js/jquery.isotope.min.js"></script>
-    <script src="js/main.js"></script>
-    <script src="js/wow.min.js"></script>
+    <script src="libs/js/jquery.js"></script>
+    <script src="libs/js/bootstrap.min.js"></script>
+    <script src="libs/js/jquery.prettyPhoto.js"></script>
+    <script src="libs/js/jquery.isotope.min.js"></script>
+    <script src="libs/js/main.js"></script>
+    <script src="libs/js/wow.min.js"></script>
 </body>
 </html>
